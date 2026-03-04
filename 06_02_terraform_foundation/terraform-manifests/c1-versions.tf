7aeb31d60b6d2d13fbc44d0c0cff64488b882f9e
vinaymamidala <mamidalaavinay@gmail.com>
2026-03-05 00:06:38 +0530

    terraform 06 02 added

---
+++ 06_02_terraform_foundation/terraform-manifests/c1-versions.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+    version = "~> 3.0"
+   }
+ }
+}
+
+#provider block
+
+provider "aws" {
+    region = "us-east-1"
+}
