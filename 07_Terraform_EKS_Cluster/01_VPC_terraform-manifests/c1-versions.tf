139c7941543f4ed7b86791a75641c1b0b3b63051
vinaymamidala <mamidalaavinay@gmail.com>
2026-03-17 20:58:01 +0530

    EKS cluster and vpc module updated

---
+++ 07_Terraform_EKS_Cluster/01_VPC_terraform-manifests/c1-versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.0"
+    }
+  }
+# Remote Backend
+  backend "s3" {
+    bucket         = "tfstate-dev-us-east-1-89c2ul"
+    key            = "vpc/dev/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    use_lockfile = true
+  }   
+}
+
+provider "aws" {
+  region = var.aws_region
+}
